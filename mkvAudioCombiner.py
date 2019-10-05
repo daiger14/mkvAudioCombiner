d7a099e5d2846cde07622e6fbfbc2cbe23136a8a
Andrei Tolcan <andrei.tolcan@it.abb.com>
2019-10-05 13:14:02 +0300

    Costumized audio video naming parsing, now you can choose name pattern for video and audio

--- mkvAudioCombiner.py
+++ mkvAudioCombiner.py
@@ -9,22 +9,49 @@ else:
   video_path = sys.argv[2]
   audio_path = sys.argv[3]
   video_files = os.listdir(video_path)
-  auddio_files = os.listdir(audio_path)
+  audio_files = os.listdir(audio_path)
+  videos_to_remove = []
+
+  for i, video in enumerate(video_files):
+    if i < 8:
+      print("{}. {}".format(i + 1, video))
+  video_name_index = int(input("Choose the right video file name. Type number:\n")) - 1
+
+  print("Parts splitted by dot:")
+  for i, parsed_video_name in enumerate(video_files[video_name_index].split(".")):
+    print("{}. Video: {} === Audio: {}".format(i + 1, parsed_video_name, audio_files[0].split(".")[i]))
+  dot_index = int(input("Choose the right part of video name which is equal with audio name by episode. Type number:\n")) - 1
+
   for video in video_files:
-    video_name = video.split(".")[0]
-    for audio in auddio_files:
-      audio_name = audio.split(".")[0]
+    try:
+      video_name = video.split(".")[dot_index]
+    except:
+      continue
+    for audio in audio_files:
+      try:
+        audio_name = audio.split(".")[dot_index]
+      except:
+        continue
       if video_name == audio_name:
-        audio_full_path = r"{}\{}".format(audio_path, audio)
-        video__full_path = r"{}\{}".format(video_path, video)
-        output_file_name = video__full_path.split(".")[0] + '_RUS' + '.mkv'
+        audio_full_path = os.path.join(audio_path, audio)
+        video_full_path = os.path.join(video_path, video)
+        output_file_name = video_full_path.split(".mkv")[0] + '_RUS.mkv'
+        videos_to_remove.append(video_full_path)
+
         if not os.path.isfile(output_file_name):
-          print("Insert audio: {} => In video: {}".format(audio_name, video_name))
-          command = """{} --ui-language en --output ^"{}^" --no-audio --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --track-order 0:0,1:0""".format(mkv_merge_path, output_file_name, video__full_path, audio_full_path)
-          # print(command)
+          print("Insert audio: {} ===>>> In video: {}".format(audio, video))
+          command = """{} --ui-language en --output ^"{}^" --no-audio --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --track-order 0:0,1:0""".format(mkv_merge_path, output_file_name, video_full_path, audio_full_path)
           os.system(command)
         else:
           print('File Already Exists!!! => {}'.format(output_file_name))
+
+  remove_originals = int(input("Remove original videos?\n1 - Yes\n0 - No\n"))
+  if remove_originals == 1:
+    print("Removing....")
+    for video in videos_to_remove:
+      print(video)
+      os.remove(video)
+  print("Done")
 
 # Example of command from mkvtoolnix GUI
 # C:/Users/User/Desktop/mkvtoolnix\mkvmerge.exe --ui-language en --output ^"
