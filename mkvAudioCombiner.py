6fd7be884e096cd4c198c4dea710b9d807444dee
Andrei Tolcan <andrei.tolcan@it.abb.com>
2020-03-17 21:56:39 +0200

    Added ffmpeg

--- mkvAudioCombiner.py
+++ mkvAudioCombiner.py
@@ -3,9 +3,12 @@ import sys
 
 
 mkv_merge_path = sys.argv[1]
-if mkv_merge_path == 'help':
-  print("First param path to mkvmerge.exe cli\nSecond param path to video folder\nThird param path to audio folder")
+if 'help' in mkv_merge_path:
+  print("First param path to mkvmerge.exe cli or write 'ffmpeg' to use it\nSecond param path to video folder\nThird param path to audio folder")
 else:
+  if len(sys.argv) != 4:
+    print("Wrong params lenght")
+    quit()
   video_path = sys.argv[2]
   audio_path = sys.argv[3]
   video_files = os.listdir(video_path)
@@ -33,15 +36,23 @@ else:
       except:
         continue
       if video_name == audio_name:
-        audio_full_path = os.path.join(audio_path, audio).replace("&", "^&") # Escape character &
-        video_full_path = os.path.join(video_path, video).replace("&", "^&")
+        if mkv_merge_path == "ffmpeg":
+          audio_full_path = os.path.join(audio_path, audio)
+          video_full_path = os.path.join(video_path, video)
+        else:   
+          audio_full_path = os.path.join(audio_path, audio).replace("&", "^&") # Escape character &
+          video_full_path = os.path.join(video_path, video).replace("&", "^&")
         output_file_name = video_full_path.split(".mkv")[0] + '_RUS.mkv'
         videos_to_remove.append(video_full_path)
 
         if not os.path.isfile(output_file_name):
           print("Insert audio: {} ===>>> In video: {}".format(audio, video))
-          command = """{} --ui-language en --output ^"{}^" --no-audio --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --track-order 0:0,1:0""".format(mkv_merge_path, output_file_name, video_full_path, audio_full_path)
-          # print(command)
+          command = None
+          if mkv_merge_path == "ffmpeg":
+            command = """{} -i '{}' -i '{}' -c:v copy -map 0:v:0 -map 1:a:0 '{}'""".format(mkv_merge_path, video_full_path, audio_full_path, output_file_name)
+          else:
+            command = """{} --ui-language en --output ^"{}^" --no-audio --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --language 0:rus --default-track 0:yes ^"^(^" ^"{}^" ^"^)^" --track-order 0:0,1:0""".format(mkv_merge_path, output_file_name, video_full_path, audio_full_path)
+          print(command)
           os.system(command)
         else:
           print('File Already Exists!!! => {}'.format(output_file_name))
